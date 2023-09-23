ad910bb74f4bd77fe8354f1042144e6fec5182ad
Edon <edon@valdman.works>
2023-09-23 14:02:49 -0500

    Update PDFDocument+UIImage Init.swift
    Made this initializer public.

--- Sources/DocumentScannerView/PDFDocument+UIImage Init.swift
+++ Sources/DocumentScannerView/PDFDocument+UIImage Init.swift
@@ -9,7 +9,7 @@ import UIKit
 import PDFKit
 
 extension PDFDocument {
-    convenience init(_ images: [UIImage]) {
+    public convenience init(_ images: [UIImage]) {
         self.init()
         for i in 0..<images.count {
             let pdfPage = PDFPage(image: images[i])
